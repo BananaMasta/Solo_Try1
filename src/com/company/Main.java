2b3b2ba04c00324a003659aec8e7eff5ac326a0f
student01 <student01@webzavod.ru>
2021-02-05 15:52:50 +0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,152 @@
+package com.company;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        chooser();
+
+
+    }
+
+    public static void addUserInfo() throws IOException {
+        Set<User> set = new HashSet<>();
+        File file = new File("C:/stud/solo.txt");
+        Scanner scanner = new Scanner(System.in);
+        User user = new User();
+        System.out.println("Введите свой ник");
+        String username = scanner.nextLine();
+        if(searchByUsername(readUserinfo(), username) != null) {
+            System.out.println("user already exists");
+        } else user.setUserName(username);
+        user.setUserName(scanner.nextLine());
+        System.out.println("Введите свою Фамилию");
+        user.setFirstName(scanner.nextLine());
+        System.out.println("Введите своё Имя");
+        user.setSecondName(scanner.nextLine());
+        System.out.println("Введите своё Отчество");
+        user.setLastName(scanner.nextLine());
+        System.out.println("Введите свою Поту");
+        user.setUserMail(scanner.nextLine());
+        System.out.println("Придумайте пароль");
+        user.setUserPassword(scanner.nextLine());
+        userIdGener(user);
+        if (file.length() == 0) {
+            Files.write(Paths.get(file.getPath()), user.toString().getBytes(), StandardOpenOption.APPEND);
+        } else {
+            String user1 = ",\n";
+            user1 += user.toString();
+            Files.write(Paths.get(file.getPath()), user1.getBytes(), StandardOpenOption.APPEND);
+        }
+        for (int i = 0; i<file.length(); i++){
+
+        }
+    }
+
+    public static void userIdGener(User user) {
+        long x = 1;
+        long y = 2356757;
+        Random random = new Random();
+        long gener = x + ((long) (random.nextDouble() * (y - x)));
+        user.setId(gener);
+    }
+
+    public static List<User> readUserinfo() throws IOException {
+        List<User> users = new ArrayList<>();
+        Map<String, String> map = new HashMap<>();
+        List<String> list = new ArrayList<>();
+        String file = "C:/stud/solo.txt";
+        Path path = Paths.get(file);
+
+        Scanner scanner = new Scanner(path);
+        scanner.useDelimiter("},\n");
+        while (scanner.hasNext()) {
+            list.add(scanner.next());
+        }
+        for (String s : list) {
+            String[] del = s.replace("\"", "").replace("{", "").replace("}", "").split(",");
+            for (int i = 0; i < del.length; i++) {
+                String[] str = del[i].split(":");
+                map.put(str[0], str[1]);
+            }
+            User user = new User();
+            user.setId(Long.parseLong(map.get("UserID")));
+            user.setUserName(map.get("UserName"));
+            user.setFirstName(map.get("FirstName"));
+            user.setSecondName(map.get("SecondName"));
+            user.setLastName(map.get("LastName"));
+            user.setUserMail(map.get("UserMail"));
+            user.setUserPassword(map.get("UserPassword"));
+            users.add(user);
+        }
+        return users;
+    }
+
+
+    public static User searchByUsername(List<User> users, String username) {
+        for (User u : users) {
+            if (u.getUserName().equals(username)) {
+                return u;
+            }
+        }
+        return null;
+    }
+
+    public static void searchByMail(List<User> users, String usermail) {
+        for (User u : users) {
+            if (u.getUserMail().equals(usermail)) {
+                System.out.println(u.getUserMail());
+            }
+
+        }
+    }
+        public static void searchByLastName(List<User> users, String userlastname){
+        for (User u : users){
+            if(u.getLastName().equals(userlastname)){
+                System.out.println(u.getLastName());
+            }
+            }
+        }
+
+    public static void printUsers(List<User> users) {
+        for (User u : users) {
+            System.out.println(u);
+        }
+    }
+
+    public static void chooser() throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        switch (scanner.nextLine()) {
+            case "add":
+                System.out.println("Добавление пользователя");
+                addUserInfo();
+                break;
+            case "read":
+                System.out.println("Все пользователи");
+                printUsers(readUserinfo());
+
+                break;
+            case "searchByUsername":
+                System.out.println("Поиск");
+                searchByUsername(readUserinfo(), scanner.nextLine());
+                break;
+            case "searchByMail":
+                System.out.println("Поиск по почте");
+                searchByMail(readUserinfo(), scanner.nextLine());
+                break;
+            case"searchByLastName":
+                System.out.println("Поиск по отчеству");
+                searchByLastName(readUserinfo(), scanner.nextLine());
+        }
+    }
+}
+
+
+
---
+++ src/com/company/User.java
@@ -0,0 +1,79 @@
+package com.company;
+
+public class User {
+    private long id;
+    private String userName;
+    private String firstName;
+    private String secondName;
+    private String lastName;
+    private String userMail;
+    private String userPassword;
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    public String getUserMail() {
+        return userMail;
+    }
+
+    public void setUserMail(String userMail) {
+        this.userMail = userMail;
+    }
+
+    @Override
+    public String toString() {
+        return "{\"UserID\":\"" + this.id + "\"," +
+                "\"UserName\":\"" + this.userName + "\"," +
+                "\"FirstName\":\"" + this.firstName + "\"," +
+                "\"SecondName\":\"" + this.secondName + "\"," +
+                "\"LastName\":\"" + this.lastName + "\"," +
+                "\"UserMail\":\"" + this.userMail + "\"," +
+                "\"UserPassword\":\"" + this.userPassword + "\"}";
+    }
+}
